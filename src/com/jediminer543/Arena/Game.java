3df20152b4d9205695ff20dabb46b24dc8e9dba8
jediminer543 <jediminer543@users.noreply.github.com>
2014-04-05 14:55:20 +0100

    It has begun - Begun work on code - Implemented config file - Added lwjgl licence

---
+++ src/com/jediminer543/Arena/Game.java
@@ -0,0 +1,15 @@
+package com.jediminer543.Arena;
+
+public class Game
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(Boolean.parseBoolean("True"));
+	}
+	
+	public static void initDisplay()
+	{
+		
+	}
+}
---
+++ src/com/jediminer543/Arena/config/Config.java
@@ -0,0 +1,104 @@
+package com.jediminer543.Arena.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config 
+{
+	Properties config;
+	
+	public Config(File f) throws IOException
+	{
+		config = new Properties();
+		FileInputStream configreader = new FileInputStream(f);
+		config.load(configreader);
+	}
+	
+	
+	/*
+	public Object read(String path) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(config));
+		String line = null;
+		while ((line = reader.readLine()) != null)
+		{
+		if (!line.startsWith("#"))
+		{
+			if (line.replaceAll("\\s+", "").startsWith(path.replaceAll("\\s+", "")))
+			{
+				for (line.length()
+			}
+		}
+		}
+		reader.close();
+		return null;
+	}
+	*/
+	
+	public void set(String path, Object value)
+	{
+		if (value instanceof String)
+		{
+		config.setProperty(path, (String) value);
+		}
+		else if (value instanceof Boolean)
+		{
+		config.setProperty(path, Boolean.toString((boolean) value));
+		}
+		else if (value instanceof Integer)
+		{
+		config.setProperty(path, Integer.toString((int) value));
+		}
+		else if (value instanceof Long)
+		{
+		config.setProperty(path, Long.toString((long) value));
+		}
+		else if (value instanceof Float)
+		{
+		config.setProperty(path, Float.toString((float) value));
+		}
+		else if (value instanceof Double)
+		{
+		config.setProperty(path, Double.toString((double) value));
+		}
+	}
+	
+	public String readString(String path)
+	{
+		return config.getProperty(path);
+	}
+	
+	public boolean readBoolean(String path)
+	{
+		String value = config.getProperty(path);
+		return Boolean.parseBoolean(value);
+	}
+	
+	public int readInteger(String path)
+	{
+		String value = config.getProperty(path);
+		return Integer.parseInt(value);
+	}
+	
+	public long readLong(String path)
+	{
+		String value = config.getProperty(path);
+		return Long.parseLong(value);
+	}
+	
+	public float readFloat(String path)
+	{
+		String value = config.getProperty(path);
+		return Float.parseFloat(value);
+	}
+	public double readDouble(String path)
+	{
+		String value = config.getProperty(path);
+		return Double.parseDouble(value);
+	}
+	
+	
+	
+}
